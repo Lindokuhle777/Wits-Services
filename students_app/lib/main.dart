5fefb24e359949d472e4b3304595a108c317ebb3
NuttyChuma <70955106+NuttyChuma@users.noreply.github.com>
2022-08-12 22:14:11 +0200

    firebase setup and google sso

--- students_app/lib/main.dart
+++ students_app/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'splash.dart';
 import 'home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(StudentsApp());
 }
 
